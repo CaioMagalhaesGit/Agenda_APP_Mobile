7c74e9c4c084e3410c47d793844805e44783db73
Caio Magalhaes <caiohenriquemagalhaes@hotmail.com>
2022-02-08 19:07:32 -0300

    Lista

--- Agenda/app/src/main/java/br/com/alura/agenda/MainActivity.java
+++ Agenda/app/src/main/java/br/com/alura/agenda/MainActivity.java
@@ -3,6 +3,8 @@ package br.com.alura.agenda;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,14 +22,9 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         List<String> students = new ArrayList<>(
-                Arrays.asList("Alex","Caio","Joao"));
-        TextView student1 = findViewById(R.id.textView);
-        TextView student2 = findViewById(R.id.textView2);
-        TextView student3 = findViewById(R.id.textView3);
-
-        student1.setText(students.get(0));
-        student2.setText(students.get(1));
-        student3.setText(students.get(2));
-
+                Arrays.asList("Alex","Caio","Joao","Levi","Jose"));
+        ListView StudentList = findViewById(R.id.activity_main_studentList);
+        StudentList.setAdapter(new ArrayAdapter<>(
+                this, android.R.layout.simple_list_item_1, students));
     }
 }
